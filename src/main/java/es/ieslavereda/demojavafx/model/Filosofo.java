06d97a08df2e54022d21f2f7ee005f91a611abe7
santi <sansolrod@alu.edu.gva.es>
2024-10-20 22:21:10 +0200

    casi bien

--- src/main/java/es/ieslavereda/demojavafx/model/Filosofo.java
+++ src/main/java/es/ieslavereda/demojavafx/model/Filosofo.java
@@ -11,32 +11,22 @@ import javafx.scene.paint.Color;
 import java.util.Arrays;
 import java.util.List;
 
-public class Filosofo extends Task<List<RegionColor>> {
+public class Filosofo {
     private Label label;
-    private Tenedor tenedor1;
-    private Tenedor tenedor2;
+    private Tenedor tenedorIzquierdo;
+    private Tenedor tenedorDerecho;
     private Thread thread;
 
-    public Filosofo(Label label, Tenedor tenedor1, Tenedor tenedor2) {
+    public Filosofo(Label label, Tenedor tenedorIzquierdo, Tenedor tenedorDerecho) {
         this.label = label;
-        this.tenedor1 = tenedor1;
-        this.tenedor2 = tenedor2;
+        this.tenedorIzquierdo = tenedorIzquierdo;
+        this.tenedorDerecho = tenedorDerecho;
     }
 
-    public Thread getThread() {
-        return thread;
-    }
-
-    public void comer(){
-       thread = new Thread(new TaskCome());
-       thread.setDaemon(true);
-       thread.start();
-
-    }
-
-    @Override
-    protected List<RegionColor> call() throws Exception {
-        return List.of();
+    public void comer() {
+        thread = new Thread(new TaskCome());
+        thread.setDaemon(true);
+        thread.start();
     }
 
     private class TaskCome extends Task<List<RegionColor>> {
@@ -44,25 +34,87 @@ public class Filosofo extends Task<List<RegionColor>> {
         public TaskCome(){
             valueProperty().addListener((observableValue, regionColor, newValue) -> {
                 for (RegionColor value : newValue) {
-                    value.getRegion().setBackground(new Background(new BackgroundFill(value.getColor(),new CornerRadii(5),new Insets(-5))));
+                    value.getRegion().setBackground(new Background(new BackgroundFill(value.getColor(), new CornerRadii(5), new Insets(-5))));
                 }
-
             });
         }
         @Override
         protected List<RegionColor> call() throws Exception {
-            while (true){
-                synchronized(tenedor1){
-                    synchronized(tenedor2){
-                        //comiendo
-                        updateValue(Arrays.asList(new RegionColor(label,Color.GREEN),new RegionColor(tenedor1.getLabel(),Color.GREEN),new RegionColor(tenedor2.getLabel(),Color.GREEN)));
-                        Thread.sleep(3000);
-                        //durmiendo
-                        updateValue(Arrays.asList(new RegionColor(label,Color.RED),new RegionColor(tenedor1.getLabel(),Color.RED),new RegionColor(tenedor2.getLabel(),Color.RED)));
-                        Thread.sleep(5000);
+            while (true) {
+                // Intentar coger el tenedor izquierdo
+                synchronized (tenedorIzquierdo) {
+                    if (tenedorIzquierdo.isDisponible()) {
+                        tenedorIzquierdo.coger();
+                        updateValue(Arrays.asList(
+                                new RegionColor(label, Color.BLUE), // Azul al coger el tenedor izquierdo
+                                new RegionColor(tenedorIzquierdo.getLabel(), Color.RED) // Tenedor izquierdo a rojo
+                        ));
+
+                        // Intentar coger el tenedor derecho
+                        synchronized (tenedorDerecho) {
+                            if (tenedorDerecho.isDisponible()) {
+                                tenedorDerecho.coger();
+
+                                // Ambos tenedores están cogidos, cambiar a verde
+                                updateValue(Arrays.asList(
+                                        new RegionColor(label, Color.GREEN), // Filósofo a verde
+                                        new RegionColor(tenedorIzquierdo.getLabel(), Color.GREEN),
+                                        new RegionColor(tenedorDerecho.getLabel(), Color.GREEN)
+                                ));
+
+                                // Tiempo de comer
+                                Thread.sleep(3000); // Simula el tiempo de comer
+
+                                // Liberar tenedores
+                                tenedorDerecho.soltar();
+                                tenedorIzquierdo.soltar();
+                            } else {
+                                // Si no se puede coger el derecho, se queda en azul
+                                updateValue(Arrays.asList(
+                                        new RegionColor(label, Color.BLUE), // Filósofo en azul
+                                        new RegionColor(tenedorIzquierdo.getLabel(), Color.RED) // Tenedor izquierdo en rojo
+                                ));
+
+                                // Mantener el tenedor izquierdo mientras espera
+                                while (!tenedorDerecho.isDisponible()) {
+                                    Thread.sleep(100); // Esperar un tiempo antes de volver a intentar
+                                }
+
+                                // Intentar coger de nuevo el tenedor derecho
+                                synchronized (tenedorDerecho) {
+                                    tenedorDerecho.coger();
+                                    updateValue(Arrays.asList(
+                                            new RegionColor(tenedorDerecho.getLabel(), Color.RED) // Tenedor derecho a rojo
+                                    ));
+
+                                    // Ambos tenedores están cogidos, cambiar a verde
+                                    updateValue(Arrays.asList(
+                                            new RegionColor(label, Color.GREEN), // Filósofo a verde
+                                            new RegionColor(tenedorIzquierdo.getLabel(), Color.GREEN),
+                                            new RegionColor(tenedorDerecho.getLabel(), Color.GREEN)
+                                    ));
+
+                                    // Tiempo de comer
+                                    Thread.sleep(3000); // Simula el tiempo de comer
+
+                                    // Liberar tenedores
+                                    tenedorDerecho.soltar();
+                                    tenedorIzquierdo.soltar();
+                                }
+                            }
+                        }
                     }
                 }
 
+                // Volver a rojo para la siesta
+                updateValue(Arrays.asList(
+                        new RegionColor(label, Color.RED), // Filósofo a rojo
+                        new RegionColor(tenedorIzquierdo.getLabel(), Color.RED),
+                        new RegionColor(tenedorDerecho.getLabel(), Color.RED)
+                ));
+
+                // Tiempo de siesta
+                Thread.sleep(5000); // Simula el tiempo de siesta
             }
         }
     }
--- src/main/java/es/ieslavereda/demojavafx/model/Tenedor.java
+++ src/main/java/es/ieslavereda/demojavafx/model/Tenedor.java
@@ -1,15 +1,46 @@
 package es.ieslavereda.demojavafx.model;
 
 import javafx.scene.control.Label;
+import javafx.scene.layout.Background;
+import javafx.scene.layout.BackgroundFill;
+import javafx.geometry.Insets;
+import javafx.scene.layout.CornerRadii;
+import javafx.scene.paint.Color;
 
 public class Tenedor {
     private Label label;
+    private boolean disponible = true;
+    private int id;
 
-    public Tenedor(Label label) {
+    public Tenedor(Label label, int id) {
         this.label = label;
+        this.id = id;
+        this.label.setBackground(new Background(new BackgroundFill(Color.TRANSPARENT, new CornerRadii(5), new Insets(-5))));
+    }
+
+    public synchronized void coger() throws InterruptedException {
+        while (!disponible) {
+            wait(); // Espera hasta que el tenedor esté disponible
+        }
+        disponible = false;
+        label.setBackground(new Background(new BackgroundFill(Color.RED, new CornerRadii(5), new Insets(-5)))); // Cambia a rojo
+    }
+
+    public synchronized void soltar() {
+        disponible = true;
+        label.setBackground(new Background(new BackgroundFill(Color.TRANSPARENT, new CornerRadii(5), new Insets(-5)))); // Vuelve a ser transparente
+        notifyAll(); // Notifica a los filósofos esperando
     }
 
     public Label getLabel() {
         return label;
+    }
+
+    public boolean isDisponible() {
+        return disponible;
+    }
+
+    public int getId() {
+        return id;
     }
 }
